646d71cd0b760b112238c807249c83dc0aa7585d
nakagawa <237s10mimi@gmail.com>
2020-01-10 12:00:00 +0900

    refactor: headers

--- apis/headers/update.php
+++ apis/headers/update.php
@@ -4,5 +4,5 @@
   header("Access-Control-Allow-Methods: PUT");
   header("Content-Type: application/json; charset=UTF-8");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  require __DIR__ . '/../classes/dbconnection.php';
+  json_decode(file_get_contents("php://input"))
 ?>
