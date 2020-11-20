7f42ac211163d46d290becd8d286be4f34af4d00
nakagawa <237s10mimi@gmail.com>
2020-11-20 12:00:00 +0900

    add charset & delete file

--- apis/database.php
+++ apis/database.php
@@ -18,7 +18,7 @@
       public function dbConnection()
       {
           try {
-              $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name, $this->db_username, $this->db_password);
+              $conn = new PDO('mysql:host='.$this->db_host.';dbname='.$this->db_name.';charset=utf8', $this->db_username, $this->db_password);
               $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
               return $conn;
           } catch (PDOExeption $e) {
