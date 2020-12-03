3d6edb4bcedf181f309458d89506253826dff0bf
nakagawa <237s10mimi@gmail.com>
2020-12-03 12:00:00 +0900

    join tables read & delete

--- apis/study_detail/read.php
+++ apis/study_detail/read.php
@@ -5,7 +5,10 @@
   $conn = $db->dbInstanceConnection();
   $message = new Messages();
 
-  $table_study = $_SERVER['T_STUDY'];
+  $table_study            = $_SERVER['T_STUDY'];
+  $table_project_category = $_SERVER['T_PROJECT_CATEGORY'];
+  $table_users            = $_SERVER['T_USER'];
+
   if(isset($_GET['id'])) {
     $post_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, [
       'options' => [
@@ -18,8 +21,35 @@
   }
 
   $sql = is_numeric($post_id)
-         ? "SELECT * FROM `$table_study` WHERE id ='$post_id'"
-         : "SELECT * FROM `$table_study`";
+        ? "SELECT s.id,
+                  s.study_time,
+                  s.project_id,
+                  s.study_detail,
+                  s.study_date,
+                  s.user_id,
+                  s.create_date,
+                  p.name AS project_category,
+                  u.user_name  
+            FROM $table_study AS s
+            LEFT OUTER JOIN $table_project_category AS p 
+            ON s.project_id = p.id
+            LEFT OUTER JOIN $table_users AS u
+            ON s.user_id = u.id
+            WHERE s.id = $post_id;"
+        : "SELECT s.id,
+                  s.study_time,
+                  s.project_id,
+                  s.study_detail,
+                  s.study_date,
+                  s.user_id,
+                  s.create_date,
+                  p.name AS project_category,
+                  u.user_name  
+            FROM $table_study AS s
+            LEFT OUTER JOIN $table_project_category AS p 
+            ON s.project_id = p.id
+            LEFT OUTER JOIN $table_users AS u
+            ON s.user_id = u.id;";
 
   $stmt = $conn->prepare($sql);
 
@@ -29,17 +59,20 @@
     $post_array = [];
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $post_data = [
-        'id'              => $row['id'],
-        'study_time'      => $row['study_time'], 
-        'project_category'  => $row['project_category'],
-        'study_detail'    => $row['study_detail'],
-        'study_date'      => $row['study_date'],
-        'user_id'         => $row['user_id'],
-        'create_date'     => $row['create_date']
+        'id'               => $row['id'],
+        'study_time'       => $row['study_time'], 
+        'project_id'       => $row['project_id'],
+        'study_detail'     => $row['study_detail'],
+        'study_date'       => $row['study_date'],
+        'user_id'          => $row['user_id'],
+        'create_date'      => $row['create_date'],
+        'project_category' => $row['project_category'],
+        'user_name'        => $row['user_name']
       ];
       array_push($post_array, $post_data);
     }
     echo json_encode($post_array);
+    
   } else {
     echo json_encode(['message' => $message->NoPostFound]);
   }
--- apis/users/delete.php
+++ apis/users/delete.php
@@ -1,32 +1,36 @@
 <?php
-  require_once __DIR__ . '/../headers/delete.php';
-  
-  $db = new CreateDBinstance();
-  $conn = $db->dbInstanceConnection();
-  $data = $db->setContent();
+require_once __DIR__ . '/../headers/delete.php';
 
-  $table_users = $_SERVER['T_USER'];
+$db = new CreateDBinstance();
+$conn = $db->dbInstanceConnection();
+$data = $db->setContent();
 
-  $msg['message'] = '';
-  $message = new Messages();
+$table_users = $_SERVER['T_USER'];
+$table_study = $_SERVER['T_STUDY'];
+$table_project_category['T_PROJECT_CATEGORY'];
 
-  $post_id = $data->id;
-  $check_post = "SELECT * FROM `$table_users` WHERE id=:post_id";
-  $delete_post = "DELETE FROM `$table_users` WHERE id=:post_id";
+$msg['message'] = '';
+$message = new Messages();
 
-  // 削除対象が存在するかどうかを検索
-  if(isset($post_id)) {
-    $check_post_stmt = $conn->prepare($check_post);
-    $check_post_stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
-    $check_post_stmt->execute();
-    if($check_post_stmt->rowCount() > 0){
-      $delete_post_stmt = $conn->prepare($delete_post);
-      $delete_post_stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
-      $msg['message'] = $delete_post_stmt->execute() ? $message->Success() : $message->Failure();
-    } else {
-      $msg['message'] = $message->InvlidId();
-    }
-    echo json_encode($msg);
-  } 
+$post_id = $data->id;
+$check_post = "SELECT * FROM $table_users WHERE id=:post_id";
+$delete_post = "DELETE $table_users" . "," . "$table_study 
+                FROM $table_users 
+                LEFT JOIN $table_study 
+                ON $table_users.id = $table_study.user_id 
+                WHERE $table_users.id=:post_id";
 
-?>
+// 削除対象が存在するかどうかを検索
+if (isset($post_id)) {
+  $check_post_stmt = $conn->prepare($check_post);
+  $check_post_stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+  $check_post_stmt->execute();
+  if ($check_post_stmt->rowCount() > 0) {
+    $delete_post_stmt = $conn->prepare($delete_post);
+    $delete_post_stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+    $msg['message'] = $delete_post_stmt->execute() ? $message->Success() : $message->Failure();
+  } else {
+    $msg['message'] = $message->InvlidId();
+  }
+  echo json_encode($msg);
+}
