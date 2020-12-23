d0df1932dc6ab3204928e31cd6eb37227d8367ed
nakagawa <237s10mimi@gmail.com>
2020-12-24 00:07:08 +0900

    add project_category/update.php

---
+++ apis/project_category/update.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__ . '/../headers/update.php';
+require_once __DIR__ . '/../functions.php';
+
+$db = new CreateDBinstance();
+$conn = $db->dbInstanceConnection();
+$data = $db->setContent();
+
+$msg['message'] = '';
+$message = new Messages();
+
+$table_project_category = $_SERVER['T_PROJECT_CATEGORY'];
+$update_query = "UPDATE `$table_project_category` SET 
+                  name = :name, 
+                  delete_flg = :delete_flg,
+                  create_date = :create_date,
+                  update_date = :update_date
+                  WHERE id = :id";
+
+// 変更対象が存在するかどうかを検索
+if (isset($data->id)) {
+  $post_id  = $data->id;
+  $get_post = "SELECT * FROM `$table_project_category` WHERE id=:post_id";
+  $get_stmt = $conn->prepare($get_post);
+  $get_stmt->bindValue(':post_id', $post_id, PDO::PARAM_INT);
+  $get_stmt->execute();
+  if ($get_stmt->rowCount() > 0) {
+    $row = $get_stmt->fetch(PDO::FETCH_ASSOC);
+    $post_name       = updateBindValue($row, $data, 'name');
+    $post_delete_flg = updateBindValue($row, $data, 'delete_flg');
+    $update_date     = updateBindValue($row, $data, 'update_date');
+    $post_update_date     = date('Y-m-d-H-i');
+    $post_create_date    = $row['create_date'];
+
+    $update_stmt = $conn->prepare($update_query);
+    $update_stmt->bindValue(':name',        htmlspecialchars(strip_tags($post_name)), PDO::PARAM_STR);
+    $update_stmt->bindValue(':delete_flg',  $post_delete_flg, PDO::PARAM_INT);
+    $update_stmt->bindValue(':update_date', $post_update_date, PDO::PARAM_STR);
+    $update_stmt->bindValue(':create_date', $post_create_date, PDO::PARAM_STR);
+    $update_stmt->bindValue(':id',          $post_id, PDO::PARAM_INT);
+  }
+  $msg['message'] = $update_stmt->execute() ? $message->Success() : $message->Failure();
+} else {
+  $msg['message'] = $message->InvlidId();
+}
+
+echo json_encode($msg);
