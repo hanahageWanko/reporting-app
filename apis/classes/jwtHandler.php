6cc02dc604dd2feae33c10cde2e8fd26ed91a14f
nakagawa <237s10mimi@gmail.com>
2020-12-28 14:09:27 +0900

    add Firebase/JWT

---
+++ apis/classes/jwtHandler.php
@@ -0,0 +1,69 @@
+<?php
+  require __DIR__.'/../jwt/JWT.php';
+  require __DIR__.'/../jwt/ExpiredException.php';
+  require __DIR__.'/../jwt/SignatureInvalidException.php';
+  require __DIR__.'/../jwt/BeforeValidException.php';
+
+  use \Firebase\JWT\JWT;
+
+  class JwtHandler
+  {
+      protected $jtw_secret;
+      protected $token;
+      protected $issuedAt;
+      protected $expire;
+      protected $jwt;
+
+      public function __construct()
+      {
+          date_default_timezone_set('Asia/Kolkata');
+          $this->issuedAt = time();
+          $this->expire = $this->issuedAt + 3600;
+          $this->jwt_secret = "this_is_my_secret";
+      }
+
+      public function _jwt_encode_data($iss, $data)
+      {
+          $this->token = [
+        "iss"  => $iss,
+        "aud"  => $iss,
+        "iat"  => $this->issuedAt,
+        "exp"  => $this->exprire,
+        "data" => $data
+      ];
+
+          $this->jwt = JWT::encode($this->token, $this->jwt_secret);
+          return $this->jwt;
+      }
+
+      protected function _errMsg($msg)
+      {
+          return [
+          "auth" => 0,
+          "message" => $msg
+        ];
+      }
+
+      public function _jwt_decode_data($jwt_token)
+      {
+          try {
+              $decode = JWT::decode($jwt_token, $this->jwt_secret, array('HS256'));
+              return [
+            "auth" => 1,
+            "data" => $decode->date
+          ];
+          } catch (\Firebase\JWT\ExpiredException $e) {
+              return $this->_errMsg($e->getMessage());
+          } catch (\Firebase\JWT\SignatureInvalidException $e) {
+              return $this->_errMsg($e->getMessage());
+          } catch (\Firebase\JWT\BeforeValidException $e) {
+              return $this->_errMsg($e->getMessage());
+          } catch (\DomainException $e) {
+              return $this->_errMsg($e->getMessage());
+          } catch (\InvalidArgumentException $e) {
+              return $this->_errMsg($e->getMessage());
+          } catch (\UnexpectedValueException $e) {
+              return $this->_errMsg($e->getMessage());
+          }
+      }
+  }
